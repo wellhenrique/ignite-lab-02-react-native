f275fbc138d1551689ae5c8fd8e30988f02380d7
wellhenrique <welissonh80@gmail.com>
2022-07-21 23:22:24 -0300

    home page;2C

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,17 @@
 import { Text, View } from "react-native";
 import React, { Component } from "react";
+import { VStack } from "native-base";
+import { Button } from "../components/Button";
 
 export default class Home extends Component {
   render() {
     return (
       <View>
         <Text>Home</Text>
+
+        <VStack>
+          <Button />
+        </VStack>
       </View>
     );
   }
